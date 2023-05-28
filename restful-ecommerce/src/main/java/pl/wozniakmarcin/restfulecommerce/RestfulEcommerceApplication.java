0d9127a000c00fa005aab93403af3b80ad3c8566
Marcin Wozniak <marcindamianwozniak@gmail.com>
2023-05-28 20:05:29 +0200

    first commit

---
+++ restful-ecommerce/src/main/java/pl/wozniakmarcin/restfulecommerce/RestfulEcommerceApplication.java
@@ -0,0 +1,13 @@
+package pl.wozniakmarcin.restfulecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulEcommerceApplication.class, args);
+	}
+
+}
---
+++ restful-ecommerce/src/test/java/pl/wozniakmarcin/restfulecommerce/RestfulEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wozniakmarcin.restfulecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
